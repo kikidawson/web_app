4ab56fa6038373bfe6fe7661772ccc8fb5c69549
Kiki Dawson <kikiadawson@gmail.com>
2020-12-07 16:25:48 +0000

    returning HTML

--- app.rb
+++ app.rb
@@ -8,4 +8,10 @@ get '/secret' do
   "Whoop whoop"
 end
 
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg?resize=750px:*'>
+  </div>"
+end
+
 set :session_secret, "Super secret"
